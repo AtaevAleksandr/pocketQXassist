649414763a71ea71578278b0c9bc7082ef95df18
Aleksandr Ataev <onlyaa94@mail.ru>
2024-01-15 10:11:24 +0300

    Initial Commit

---
+++ pocketQXassist/pocketQXassistApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocketQXassistApp.swift
+//  pocketQXassist
+//
+//  Created by Aleksandr Ataev on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct pocketQXassistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
